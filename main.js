acd7942ea8b34058cd852019ebac7db4f1cc4d3b
alexander-helling <53431079+alexander-helling@users.noreply.github.com>
2021-04-30 00:55:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,115 @@ var cardsArray = [
   {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
   {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
 ];
+
+// Duplicate cardsArray to create a match for each card
+var gameGrid = cardsArray.concat(cardsArray);
+
+// Randomize game grid on each load
+gameGrid.sort(function() {
+  return 0.5 - Math.random();
+})
+
+// Grab the div with an id of game-board and assign to a variable game
+var game = document.getElementById('game-board');
+// Create a section element and assign it to variable grid
+var grid = document.createElement('section');
+// Give section element a class of grid.
+grid.setAttribute('class', 'grid');
+// Append the grid section to the game-board div
+game.appendChild(grid);
+
+// Loop through each item in our cards array
+for (i = 0; i < gameGrid.length; i++) {
+  // create a div element and assign to variable card
+  var card = document.createElement('div');
+  // Apply a card class to that div
+  card.classList.add('card');
+  // Set the data-name attribute of the div to the cardsArray name
+  card.dataset.name = gameGrid[i].name;
+  // Apply the background image of the div to the cardsArray image
+  //card.style.backgroundImage = `url(${gameGrid[i].img})`;
+  // Append the div to the grid section
+  //grid.appendChild(card);
+
+  //create front of card
+  var front = document.createElement('div');
+  front.classList.add('front');
+
+  //create back of card
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${gameGrid[i].img})`
+
+  // Append card to grid
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+}
+
+var firstGuess = '';
+var secondGuess = '';
+
+//set count to 0, previous target, and delay
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
+
+// Add match CSS
+var match = function() {
+var selected = document.querySelectorAll('.selected');
+//loop through the array like ojbect containing 'selected class
+for (i=0; i < selected.length; i++){
+  selected[i].classList.add('match');
+}
+};
+
+// Reset guess after two attempts
+var resetGuesses = function() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  for (i=0; i < selected.length; i++) {
+    selected[i].classList.remove('selected');
+  }
+}
+
+
+// Add event listenter to grid
+grid.addEventListener('click', function(event){
+  // Declare variable to target our clicked item
+  var clicked = event.target;
+  //Do not allow the grid section itselft to be selected;
+  //only select div inside the grid || unmatched || unselected
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('match')||clicked.parentNode.classList.contains('selected')) {
+    return;
+  }
+  // we only wan to add 'selected class if the current count is less than 2
+  if (count < 2) {
+    count++;
+    
+    if (count === 1) {
+      //Assign first guess
+      firstGuess = clicked.parentNode.dataset.name;
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      clicked.parentNode.classList.add('selected');
+    }
+    // If both guesses are not empty
+    if (firstGuess !== '' && secondGuess !== ''){
+      if (firstGuess === secondGuess) {
+        //run the match function
+        setTimeout(match, delay);
+        setTimeout(resetGuesses, delay);
+      } else {
+        setTimeout(resetGuesses, delay);
+      }
+    }
+    previousTarget = clicked;
+  }
+});
+
